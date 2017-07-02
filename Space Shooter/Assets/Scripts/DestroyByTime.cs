f83a759c6da46fb2fe906c9d6908024a05816d2b
DESKTOP-UH2T7E9\Escher <iconescher@gmail.com>
2017-07-02 21:57:24 +0200

    2° parte finita

---
+++ Space Shooter/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float lifetime;
+
+	void start ()
+	{
+		Destroy (gameObject, lifetime);
+	}
+
+}
